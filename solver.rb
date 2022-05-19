6775d8e32a8b455606833222c300aa2434f4ed35
awaisanwar544 <awaisanwar544@gmail.com>
2022-05-20 02:50:23 +0500

    Add reverse method

--- solver.rb
+++ solver.rb
@@ -1,8 +1,12 @@
 class Solver
   def factorial(number)
-    raise Exception('Number must be 0 or bositive') if number.negative?
+    raise Exception('Number must be 0 or positive') if number.negative?
     return 1 if number <= 1
 
     factorial(number - 1) * number
+  end
+
+  def reverse(word)
+    word.reverse
   end
 end
